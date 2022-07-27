b42f6b8fa0b3282ebfedb0462f017d766be88b6e
Knucklessg1 <knucklessg1@gmail.com>
2022-07-27 00:40:28 -0500

    Checkin progress

--- media_manager/media_manager.py
+++ media_manager/media_manager.py
@@ -12,25 +12,133 @@ import ffmpeg
 class MediaManager:
 
     def __init__(self):
-        self.links = []
+        self.media_files = []
         self.download_directory = f'{os.path.expanduser("~")}/Downloads'
+        self.folder_name = ""
+        self.movie_filters = {
+            f"2160p.*$": f"2160p",
+            f"1080p.*$": f"1080p",
+            f"720p.*$": f"720p",
+            f"480p.*$": f"480p",
+            f"ARROW.": f"",
+            f"REMASTERED.": f"",
+            f"REMASTER.": f"",
+            f"RESTORED.": f"",
+            f"UNCUT.": f"",
+            f"UNRATED.": f"",
+            f"EXTENDED.": f"",
+            f"PROPER.": f"",
+            f"THEATRICAL.": f"",
+            f"CRITERION.": f"",
+            f"Final.Cut.": f"",
+            f"GERMAN": f"German",
+            f"SPANISH": f"Spanish",
+            f"SWEDISH": f"Swedish",
+            f"FRENCH": f"French",
+            f"JAPANESE": f"Japanese",
+            f"CHINESE": f"Chinese",
+            f"KOREAN": f"Korean",
+            f"ITALIAN": f"Italian",
+            f"RUSSIAN": f"Russian",
+            f"\[.*\]": f"",
+            f"^ ": f"",
+            f"\.": f" ",
+        }
+        self.series_filters = {
+            f"2160p.*$": f"",
+            f"1080p.*$": f"",
+            f"720p.*$": f"",
+            f"480p.*$": f"",
+            f"ARROW.": f"",
+            f"REMASTERED.": f"",
+            f"REMASTER.": f"",
+            f"RESTORED.": f"",
+            f"UNCUT.": f"",
+            f"UNRATED.": f"",
+            f"EXTENDED.": f"",
+            f"PROPER.": f"",
+            f"THEATRICAL.": f"",
+            f"CRITERION.": f"",
+            f"Final.Cut.": f"",
+            f"GERMAN": f"German",
+            f"SPANISH": f"Spanish",
+            f"SWEDISH": f"Swedish",
+            f"FRENCH": f"French",
+            f"JAPANESE": f"Japanese",
+            f"CHINESE": f"Chinese",
+            f"KOREAN": f"Korean",
+            f"ITALIAN": f"Italian",
+            f"RUSSIAN": f"Russian",
+            f"\[.*\]": f"",
+            f"^ ": f"",
+            f"\.": f" ",
+            r"(s[0-9][0-9]*e[0-9][0-9]*).*$": r"- \1",
+            r"(S[0-9][0-9]*E[0-9][0-9]*).*$": r"- \1",
+            r"s([0-9][0-9]*)e([0-9][0-9]*)": r"S\1E\2"
+        }
 
-        in_file = ffmpeg.input('input.mp4')
-        overlay_file = ffmpeg.input('overlay.png')(ffmpeg.concat(in_file.trim(start_frame=10, end_frame=20),
-                                                                 in_file.trim(start_frame=30, end_frame=40), ).overlay(
-        overlay_file.hflip()).drawbox(50, 50, 120, 120, color='red', thickness=5).output('out.mp4').run())
+        # in_file = ffmpeg.input('input.mp4')
+        # overlay_file = ffmpeg.input('overlay.png')(ffmpeg.concat(in_file.trim(start_frame=10, end_frame=20),
+        #                                                          in_file.trim(start_frame=30, end_frame=40), ).overlay(
+        # overlay_file.hflip()).drawbox(50, 50, 120, 120, color='red', thickness=5).output('out.mp4').run())
 
-    def subtitle_manager(self):
-        ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height)).output(out_filename, pix_fmt='yuv420p').overwrite_output()
+    def clean_media(self):
+        parent_directory = ""
+        for media_file_path in self.media_files:
+            directory = os.path.dirname(media_file_path)
+            media_file = os.path.basename(media_file_path)
+            file_name, file_extension = os.path.splitext(media_file)
 
-    def open_file(self, file):
-        youtube_urls = open(file, 'r')
-        for url in youtube_urls:
-            self.links.append(url)
-        self.links = list(dict.fromkeys(self.links))
+            if bool(re.search("S[0-9][0-9]*E[0-9][0-9]*", file_name)) or bool(re.search("s[0-9][0-9]*e[0-9][0-9]*", file_name)):
+                filters = self.series_filters
+                series = True
+                print("Series is True")
+            else:
+                filters = self.movie_filters
+                series = False
+                print("Series is False")
 
-    def get_save_path(self):
-        return self.download_directory
+            new_file_name = file_name
+            for key in filters:
+                new_file_name = re.sub(str(key), str(filters[key]), new_file_name)
+                print(f"FILENAME: {new_file_name}")
+
+            if series:
+                self.folder_name = re.sub(" - S[0-9][0-9]*E[0-9][0-9]*", "", new_file_name)
+                print(f"SERIES NAME: {self.folder_name}")
+            else:
+                self.folder_name = new_file_name
+                print(f"FOLDER NAME: {self.folder_name}")
+            print(f"Directory: {directory} - Filename: {new_file_name} - Extension: {file_extension}")
+            # Rename directory
+            parent_directory = os.path.dirname(os.path.normpath(directory))
+            os.rename(f"{directory}", f"{parent_directory}/{self.folder_name}")
+            # Rename file
+            new_media_file_path = f"{parent_directory}/{self.folder_name}/{new_file_name}{file_extension}"
+            os.rename(f"{parent_directory}/{self.folder_name}/{file_name}{file_extension}", new_media_file_path)
+
+        # Rediscover cleaned media
+        self.find_media(directory=f"{parent_directory}/{self.folder_name}")
+
+    def find_media(self, directory):
+        self.reset_media_list()
+        for file in os.listdir(directory):
+            if file.endswith(".mp4") or file.endswith(".mkv"):
+                self.media_files.append(os.path.join(directory, file))
+                print(os.path.join(directory, file))
+
+    def reset_media_list(self):
+        self.media_files = []
+
+    def subtitle_manager(self, media_file, subtitle_file):
+        ffmpeg.input(media_file, srt=subtitle_file, langauage='eng', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height)).output(out_filename, pix_fmt='yuv420p').overwrite_output()
+
+        """
+        # MP4
+        ffmpeg -i "${file}" -f srt -i "${subtitle_files[0]}" -c:v copy -c:a copy -c:s mov_text -metadata:s:s:0 language=eng "${file::-4}-output.${file_type}"
+        # MKV
+        ffmpeg -i "${file}" -f srt -i "${subtitle_files[0]}" -c:v copy -c:a copy -c:s srt -metadata:s:s:0 language=eng "${file::-4}-output.${file_type}"
+        """
 
     def set_save_path(self, download_directory):
         self.download_directory = download_directory
@@ -82,4 +190,7 @@ def main():
 
 
 if __name__ == "__main__":
-    media_manager(sys.argv[1:])
+    #media_manager(sys.argv[1:])
+    media_manager_instance = MediaManager()
+    media_manager_instance.find_media(directory="/home/mrdr/Desktop/Vincenzo")
+    #media_manager_instance.clean_media()
