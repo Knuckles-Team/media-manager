d4c86abbeac4be91ff594c4394b7cd635effe3f6
Audel Rouhi <knucklessg1@gmail.com>
2022-11-27 14:57:37 -0600

    patch updated git url

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description=description,
     long_description=f'{readme}',
     long_description_content_type='text/markdown',
-    url='https://github.com/Knucklessg1/media-manager',
+    url='https://github.com/Knuckles-Team/media-manager',
     author=__author__,
     author_email='knucklessg1@gmail.com',
     license='Unlicense',
