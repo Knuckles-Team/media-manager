1c38a29320bdac7a28820eb46613f47ef4b4fca1
AUdel <knucklessg1@gmail.com>
2022-12-23 13:00:08 -0600

    Use requirements.txt for instal_reqs in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@
 from setuptools import setup
 from media_manager.version import __version__, __author__
 from pathlib import Path
+import os
 import re
-
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 readme = Path('README.md').read_text()
 version = __version__
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=PipSession())
 readme = re.sub(r"Version: [0-9]*\.[0-9]*\.[0-9][0-9]*", f"Version: {version}", readme)
-print(f"README: {readme}")
 with open("README.md", "w") as readme_file:
     readme_file.write(readme)
 description = 'Manage your media!'
@@ -27,7 +29,7 @@ setup(
     license='Unlicense',
     packages=['media_manager'],
     include_package_data=True,
-    install_requires=['ffmpeg-python>=0.2.0'],
+    install_requires=[str(requirement.requirement) for requirement in requirements],
     py_modules=['media_manager'],
     package_data={'media_manager': ['media_manager']},
     classifiers=[
@@ -38,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points={'console_scripts': ['media-manager = media_manager.media_manager:main']},
 )
