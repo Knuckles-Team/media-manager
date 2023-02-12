40dfcd868b50a6ee6545714427dbdf1a32fe0f88
Audel Rouhi <knucklessg1@gmail.com>
2023-02-12 17:53:50 -0600

    Check the name of the folder.

--- media_manager/media_manager.py
+++ media_manager/media_manager.py
@@ -126,32 +126,33 @@ class MediaManager:
             self.new_file_name = re.sub(str(key), str(self.filters[key]), self.new_file_name)
 
     def verify_parent_directory(self):
+        if self.series:
+            self.folder_name = re.sub(" - S[0-9]+E[0-9]+", "", self.new_file_name)
+        else:
+            self.folder_name = self.new_file_name    
         # Check if media file does not have it's own folder, and create it if it does not
         print(f"\tVerifying if Media is Missing Parent Folder:\n\t\t{os.path.normpath(os.path.join(self.directory, ''))}\n\t\t{os.path.normpath(os.path.join(self.parent_directory, self.folder_name, ''))}")
-        if self.directory == os.path.join(self.parent_directory, self.folder_name):
+        if os.path.basename(self.directory) != self.folder_name:
             # If parent folder does not exist, create it
-            self.parent_directory = os.path.normpath(os.path.join(self.parent_directory, self.folder_name, ''))
-            if self.series:
-                self.folder_name = re.sub(" - S[0-9]+E[0-9]+", "", self.new_file_name)
-            else:
-                self.folder_name = self.new_file_name       
-            os.makedirs(os.path.join(self.parent_directory, self.folder_name))     
-            print(f"Created new parent directory: {os.path.join(self.parent_directory, self.folder_name)}")
-            for file_name in os.listdir(self.directory):
-                # Only move files that belong to the newly created folder
-                if self.folder_name in file_name:
-                    # construct full file path
-                    source = os.path.join(self.directory, file_name)
-                    destination = os.path.join(self.parent_directory, self.folder_name, file_name)
-                    # move only files
-                    if os.path.isfile(source):
-                        shutil.move(source, destination)
-            if os.path.isdir(os.path.join(self.directory, 'Subs')):
-                    subtitles = glob.glob(os.path.join(self.directory, 'Subs/*/'), recursive=True)
+            self.parent_directory = os.path.normpath(os.path.join(self.parent_directory, self.folder_name, ''))            
+            if os.path.isdir(os.path.normpath(os.path.join(self.parent_directory, self.folder_name))):
+                for file_name in os.listdir(self.directory):
+                    if self.folder_name in file_name:
+                        # construct full file path
+                        source = os.path.normpath(os.path.join(self.directory, file_name))
+                        destination = os.path.normpath(os.path.join(self.parent_directory, self.folder_name, file_name))
+                        # move only files
+                        if os.path.isfile(source):
+                            shutil.move(source, destination)
+                if os.path.isdir(os.path.normpath(os.path.join(self.directory, "Subs"))):
+                    subtitles = glob.glob(f"{self.directory}/Subs/*/", recursive=True)
                     for subtitle_directory in subtitles:
-                        shutil.move(f"{subtitle_directory}", f"{os.path.join(self.directory, 'Subs')}")
-                    os.rmdir(f"{os.path.join(self.directory, 'Subs')}")
-                    os.rmdir(f"{self.directory}")    
+                        shutil.move(f"{subtitle_directory}", os.path.normpath(os.path.join(self.parent_directory, self.folder_name, "Subs")))
+                    os.rmdir(os.path.normpath(os.path.join(self.directory, "Subs")))
+                    os.rmdir(f"{self.directory}") 
+            else:   
+                os.makedirs(os.path.join(self.parent_directory, self.folder_name))     
+            print(f"Created new parent directory: {os.path.join(self.parent_directory, self.folder_name, '')}")
         self.directory = self.parent_directory
 
     # Rediscover cleaned media
