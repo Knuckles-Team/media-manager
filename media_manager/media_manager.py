16ffe735e86cfa33eacce8799ccd36e72aa787d6
knucklessg1 <knucklessg1@gmail.com>
2023-08-25 23:37:16 -0500

    Video codec scanned for is hevc.

--- media_manager/media_manager.py
+++ media_manager/media_manager.py
@@ -412,7 +412,7 @@ class MediaManager:
             video_codec = ""
             self.print(f"Error reading metadata: {e}")
         current_index = self.media_file_index
-        if (current_title_metadata != self.new_file_name or (self.optimize and video_codec != "hvec")) and self.subtitle is False:
+        if (current_title_metadata != self.new_file_name or (self.optimize and video_codec != "hevc")) and self.subtitle is False:
             try:
                 ffmpeg.input(self.new_media_file_path) \
                     .output(self.temporary_media_file_path, **self.output_parameters) \
@@ -430,7 +430,7 @@ class MediaManager:
             os.remove(self.new_media_file_path)
             os.rename(self.temporary_media_file_path, self.new_media_file_path)
             self.media_file_index = 0
-        elif (current_title_metadata != self.new_file_name or (self.optimize and video_codec != "hvec")) and self.subtitle is True:
+        elif (current_title_metadata != self.new_file_name or (self.optimize and video_codec != "hevc")) and self.subtitle is True:
             subtitle_file = "English.srt"
             subtitle_files = []
             if self.media_type == "series" and os.path.isdir(f"{self.parent_directory}/{self.folder_name}/Subs"):
